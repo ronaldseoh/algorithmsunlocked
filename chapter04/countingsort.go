b9fa5cbf912198df812be262df17a490a5eb22a3
Ronald Seoh <ronaldseoh@icloud.com>
2019-06-13 15:16:00 +0900

    chapter04: counting sort comments

--- chapter04/countingsort.go
+++ chapter04/countingsort.go
@@ -3,12 +3,23 @@
 package chapter04
 
 // CountingSort is a generalization of ideas from ReallySimpleSort.
+// Given that we have n elements in A where they are known to
+// have values in the range of [0..m-1], we can do sorting on A
+// without ever comparing sort keys of any elements.
+// This algorithm boasts Theta(m+n) time, which beats
+// a lower bound of Theta(n * lg n) of comparsion sort algorithms.
+// Note that this algorithm won't work if sort keys are not integers.
 func CountingSort(A []int, n int, m int) []int {
 
+	// First, count # of elements with each integer sort keys
 	equal := countKeysEqual(A, n, m)
 
+	// Second, count # of elements that have sort keys less than
+	// each key. Do this by cumultatively adding up values of `equal`.
 	less := countKeysLess(equal, m)
 
+	// Finally, re-assign values to A using the # of elements with
+	// specific keys
 	B := rearrange(A, less, n, m)
 
 	return B
@@ -19,6 +30,8 @@ func rearrange(A []int, less []int, n int, m int) []int {
 	B := make([]int, n-1)
 	next := make([]int, m)
 
+	// next contains where the next encountered elements
+	// with the value j should go in A.
 	for j := 0; j <= m-1; j++ {
 		next[j] = less[j] + 1
 	}
