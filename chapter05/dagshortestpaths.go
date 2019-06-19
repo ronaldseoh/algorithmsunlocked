880c2ee2eebab9532246db4b47f57a01d0815cdd
Ronald Seoh <ronaldseoh@icloud.com>
2019-06-19 15:29:37 +0900

    chapter05: dag shortest paths comments

--- chapter05/dagshortestpaths.go
+++ chapter05/dagshortestpaths.go
@@ -11,16 +11,25 @@ func DagShortestPaths(G *Dag, sourceVertex int) []int {
 	l := TopologicalSort(G)
 
 	shortest := make([]int, G.Length)
+
+	// predecessor of v on a shortest path from s to v: a vertex u
+	// such that a shortest path from s to v is path from s to u
+	// and then a single edge (u, v).
 	pred := make([]int, G.Length)
 
+	// We don't need to go anywhere to reach sourceVertex
 	shortest[sourceVertex] = 0
 
+	// We know nothing at all (yet) about how we should reach other destinations
+	// from sourceVertex, so we initialize shortest[i] with infinity
+	// (max integer value here for implementation.)
 	for i := 0; i < G.Length; i++ {
 		if i != sourceVertex {
-			shortest[i] = int(^uint(0) >> 1)
+			shortest[i] = int(^uint(0) >> 1) // maximum value allowed for signed integers
 		}
 	}
 
+	// Apply relaxation steps to each vertex and their directed edges.
 	for _, vertex := range l {
 		for _, destination := range G.Edges[vertex] {
 			relax(G, shortest, pred, vertex, destination)
@@ -31,6 +40,9 @@ func DagShortestPaths(G *Dag, sourceVertex int) []int {
 }
 
 func relax(G *Dag, shortest []int, pred []int, u int, v int) {
+	// If visiting v through u is found to be a shorter route than
+	// the previously known shortest route, then assign the newly known
+	// shortest weight to shortest[v], and make u to be predecessor of v.
 	if shortest[u]+G.Weights[u][v] < shortest[v] {
 		shortest[v] = shortest[u] + G.Weights[u][v]
 		pred[v] = u
