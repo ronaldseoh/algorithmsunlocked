367921f502eb98786d9faadc37f8d892cb6f7856
Ronald Seoh <ronaldseoh@icloud.com>
2019-06-27 14:25:25 +0900

    chapter06: The Bellman-Ford Algorithm

---
+++ chapter06/bellmanford.go
@@ -0,0 +1,49 @@
+// Package chapter06 contains
+// implementations of the algorithms introduced in Chapter 6.
+package chapter06
+
+// BellmanFord is an implementation of the Bellman-Ford algorithm.
+func BellmanFord(G *DiGraph, sourceVertex int) ([]int, []int) {
+
+	// shortest store the weight values of the shortest paths from
+	// sourceVertex to other vertices in G.
+	shortest := make([]int, G.Length)
+
+	// predecessor of v on a shortest path from s to v: a vertex u
+	// such that a shortest path from s to v is path from s to u
+	// and then a single edge (u, v).
+	pred := make([]int, G.Length)
+
+	// We know nothing at all (yet) about how we should reach other destinations
+	// from sourceVertex, so we assign each Key with infinity,
+	// except for sourceVertex itself.
+	// (max integer value here for implementation.)
+	for i := 0; i < G.Length; i++ {
+		if i == sourceVertex {
+			// if i == sourceVerttex, sp(s, s) == 0
+			shortest[i] = 0
+			// While the original pseudocode only uses shortest[] to keep track of
+			// sp(s, v), we store sp(s, v) separately to G.Vertices[i] as well
+			G.Vertices[i].Key = 0
+		} else {
+			shortest[i] = int(^uint(0) >> 1) // maximum value allowed for signed integers
+			G.Vertices[i].Key = int(^uint(0) >> 1)
+		}
+	}
+
+	// Apply relaxation steps to each vertex and their directed edges, exactly n-1 times.
+	for i := 1; i <= G.Length-1; i++ {
+		for j := 0; j < G.Length; j++ {
+			// Remove the shortest vertex found from Q
+			vertex := G.Vertices[j]
+
+			// Relax all the edges that depart from the current vertex
+			for _, destination := range G.Edges[vertex] {
+				Relax(G, shortest, pred, vertex.Value.(int), destination.Value.(int))
+				destination.Key = shortest[destination.Value.(int)]
+			}
+		}
+	}
+
+	return shortest, pred
+}
---
+++ chapter06/bellmanford_test.go
@@ -0,0 +1,70 @@
+package chapter06
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBellmanFord(t *testing.T) {
+
+	const testDAGLength int = 5
+
+	testDAG := &DiGraph{
+		Length:   testDAGLength,
+		Vertices: make([]*Element, testDAGLength),
+		Edges:    make(map[*Element][]*Element),
+		Weights:  make(map[*Element]map[*Element]int),
+	}
+
+	for i := 0; i < testDAGLength; i++ {
+		vertex := &Element{
+			Key:   int(^uint(0) >> 1),
+			Value: i,
+		}
+
+		testDAG.Vertices[i] = vertex
+
+		testDAG.Weights[vertex] = make(map[*Element]int)
+	}
+
+	testDAG.Edges[testDAG.Vertices[0]] = append(testDAG.Edges[testDAG.Vertices[0]], testDAG.Vertices[1])
+
+	testDAG.Weights[testDAG.Vertices[0]][testDAG.Vertices[1]] = 6
+
+	testDAG.Edges[testDAG.Vertices[0]] = append(testDAG.Edges[testDAG.Vertices[0]], testDAG.Vertices[2])
+
+	testDAG.Weights[testDAG.Vertices[0]][testDAG.Vertices[2]] = 7
+
+	testDAG.Edges[testDAG.Vertices[1]] = append(testDAG.Edges[testDAG.Vertices[1]], testDAG.Vertices[2])
+
+	testDAG.Weights[testDAG.Vertices[1]][testDAG.Vertices[2]] = 8
+
+	testDAG.Edges[testDAG.Vertices[1]] = append(testDAG.Edges[testDAG.Vertices[1]], testDAG.Vertices[3])
+
+	testDAG.Weights[testDAG.Vertices[1]][testDAG.Vertices[3]] = 5
+
+	testDAG.Edges[testDAG.Vertices[1]] = append(testDAG.Edges[testDAG.Vertices[1]], testDAG.Vertices[4])
+
+	testDAG.Weights[testDAG.Vertices[1]][testDAG.Vertices[4]] = -4
+
+	testDAG.Edges[testDAG.Vertices[2]] = append(testDAG.Edges[testDAG.Vertices[2]], testDAG.Vertices[3])
+
+	testDAG.Weights[testDAG.Vertices[2]][testDAG.Vertices[3]] = -3
+
+	testDAG.Edges[testDAG.Vertices[2]] = append(testDAG.Edges[testDAG.Vertices[2]], testDAG.Vertices[4])
+
+	testDAG.Weights[testDAG.Vertices[2]][testDAG.Vertices[4]] = 9
+
+	testDAG.Edges[testDAG.Vertices[3]] = append(testDAG.Edges[testDAG.Vertices[3]], testDAG.Vertices[1])
+
+	testDAG.Weights[testDAG.Vertices[3]][testDAG.Vertices[1]] = -2
+
+	testDAG.Edges[testDAG.Vertices[4]] = append(testDAG.Edges[testDAG.Vertices[4]], testDAG.Vertices[0])
+
+	testDAG.Weights[testDAG.Vertices[4]][testDAG.Vertices[0]] = 8
+
+	shortest, pred := BellmanFord(testDAG, 0)
+
+	fmt.Println(shortest)
+	fmt.Println(pred)
+}
