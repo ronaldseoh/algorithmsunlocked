b7449affc6fdf521186132c086a5027d842465d5
Ronald Seoh <ronaldseoh@icloud.com>
2019-06-27 16:23:30 +0900

    chapter06: FindNegativeWeightCycle WIP

---
+++ chapter06/findnegativeweightcycle.go
@@ -0,0 +1,45 @@
+// Package chapter06 contains
+// implementations of the algorithms introduced in Chapter 6.
+package chapter06
+
+import "container/list"
+
+// FindNegativeWeightCycle is an algorithm designed to
+// detect negative weight cycles in any directed graphs.
+func FindNegativeWeightCycle(G *DiGraph, shortest []int, pred []int) *list.List {
+
+	var v *Element
+
+	for _, source := range G.Vertices {
+		for _, destination := range G.Edges[source] {
+			if shortest[source.Value.(int)]+G.Weights[source][destination] < shortest[destination.Value.(int)] {
+				v = destination
+			}
+		}
+	}
+
+	cycle := list.New()
+
+	if v != nil {
+		visited := make(map[*Element]bool)
+
+		x := v
+
+		for visited[x] == false {
+			visited[x] = true
+			x = G.Vertices[pred[x.Value.(int)]]
+		}
+
+		v = G.Vertices[pred[x.Value.(int)]]
+
+		cycle.PushBack(x)
+
+		for v != x {
+			cycle.PushFront(v)
+			v = G.Vertices[pred[v.Value.(int)]]
+		}
+
+	}
+
+	return cycle
+}
