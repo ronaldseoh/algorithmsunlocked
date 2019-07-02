e6769329bf4b4f1767a55720259d41fbbd0ff823
Ronald Seoh <ronaldseoh@icloud.com>
2019-07-02 13:47:11 +0900

    chapter06: floyd-warshall - changed loop variables to match the book's pseudocode

--- chapter06/floydwarshall.go
+++ chapter06/floydwarshall.go
@@ -8,27 +8,27 @@ func FloydWarshall(G *DiGraph) (map[int][][]int, map[int][][]int) {
 	shortest := make(map[int][][]int)
 	pred := make(map[int][][]int)
 
-	for i := -1; i < G.Length; i++ {
-		shortest[i] = make([][]int, G.Length)
-		pred[i] = make([][]int, G.Length)
+	for x := -1; x < G.Length; x++ {
+		shortest[x] = make([][]int, G.Length)
+		pred[x] = make([][]int, G.Length)
 
-		for j := 0; j < G.Length; j++ {
-			shortest[i][j] = make([]int, G.Length)
-			pred[i][j] = make([]int, G.Length)
+		for u := 0; u < G.Length; u++ {
+			shortest[x][u] = make([]int, G.Length)
+			pred[x][u] = make([]int, G.Length)
 		}
 	}
 
-	for j := 0; j < G.Length; j++ {
-		for k := 0; k < G.Length; k++ {
-			if j == k {
-				shortest[-1][j][k] = 0
-				pred[-1][j][k] = -1
-			} else if val, ok := G.Weights[G.Vertices[j]][G.Vertices[k]]; ok {
-				shortest[-1][j][k] = val
-				pred[-1][j][k] = j
+	for u := 0; u < G.Length; u++ {
+		for v := 0; v < G.Length; v++ {
+			if u == v {
+				shortest[-1][u][v] = 0
+				pred[-1][u][v] = -1
+			} else if val, ok := G.Weights[G.Vertices[u]][G.Vertices[v]]; ok {
+				shortest[-1][u][v] = val
+				pred[-1][u][v] = u
 			} else {
-				shortest[-1][j][k] = int(^uint(0) >> 1)
-				pred[-1][j][k] = -1
+				shortest[-1][u][v] = int(^uint(0) >> 1)
+				pred[-1][u][v] = -1
 			}
 		}
 	}
