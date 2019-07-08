bbf5f2a9e8216797c98313e3c32a7998dc5c6ddf
Ronald Seoh <ronaldseoh@icloud.com>
2019-07-08 10:22:25 +0900

    chapter07: computetransformtables test - changed the test case and better output printing

--- chapter07/computetransformtables_test.go
+++ chapter07/computetransformtables_test.go
@@ -7,19 +7,29 @@ import (
 
 func TestComputeTransformTables(t *testing.T) {
 
-	testString1 := "CATCGA"
-	testString2 := "GTACCGTCA"
+	testString1 := "ACAAGC"
+	testString2 := "CCGT"
 
 	testCopyCost := -1
 	testReplaceCost := 1
 	testDeleteCost := 2
 	testInsertCost := 2
 
-	costs, ops := ComputeTransformTables(testString1, testString2, testCopyCost, testReplaceCost, testDeleteCost, testInsertCost)
+	costs, ops := ComputeTransformTables(
+		testString1, testString2, testCopyCost, testReplaceCost, testDeleteCost, testInsertCost,
+	)
 
 	fmt.Println("costs")
 	for i := range costs {
-		fmt.Println(costs[i])
+		fmt.Print("[ ")
+		for _, b := range costs[i] {
+			if b == int(^uint(0)>>1)/2 {
+				fmt.Print("Inf ")
+			} else {
+				fmt.Printf("%*d ", 3, b)
+			}
+		}
+		fmt.Printf("]\n")
 	}
 	fmt.Println()
 
