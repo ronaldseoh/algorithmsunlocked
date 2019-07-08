c42a26b328f7ce7fbcba4664703a70a39068a17e
Ronald Seoh <ronaldseoh@icloud.com>
2019-07-08 10:21:01 +0900

    chapter07: ComputeTransformTables - report each operation with alphabets instead of integers

--- chapter07/computetransformtables.go
+++ chapter07/computetransformtables.go
@@ -16,44 +16,45 @@ func ComputeTransformTables(
 	costs := make([][]int, len(X)+1)
 
 	// the last operation applied for X_i -> Y_j problem.
-	// 1: copy, 2: replace, 3: delete, 4: insert
-	ops := make([][]int, len(X)+1)
+	// "C": copy, "R": replace, "D": delete, "I": insert
+	ops := make([][]string, len(X)+1)
 
 	// Initialize
 	costs[0] = make([]int, len(Y)+1)
-	ops[0] = make([]int, len(Y)+1)
+	ops[0] = make([]string, len(Y)+1)
 	costs[0][0] = 0
+	ops[0][0] = " " // No operation
 
 	for i := 1; i <= len(X); i++ {
 		costs[i] = make([]int, len(Y)+1)
-		ops[i] = make([]int, len(Y)+1)
+		ops[i] = make([]string, len(Y)+1)
 		costs[i][0] = i * costDelete
-		ops[i][0] = 3
+		ops[i][0] = "D"
 	}
 
 	for j := 1; j <= len(Y); j++ {
 		costs[0][j] = j * costInsert
-		ops[0][j] = 4
+		ops[0][j] = "I"
 	}
 
 	for i := 1; i <= len(X); i++ {
 		for j := 1; j < len(Y); j++ {
 			if X[i-1] == Y[j-1] {
 				costs[i][j] = costs[i-1][j-1] + costCopy
-				ops[i][j] = 1
+				ops[i][j] = "C"
 			} else {
 				costs[i][j] = costs[i-1][j-1] + costReplace
-				ops[i][j] = 2
+				ops[i][j] = "R"
 			}
 
 			if costs[i-1][j]+costDelete < costs[i][j] {
 				costs[i][j] = costs[i-1][j] + costDelete
-				ops[i][j] = 3
+				ops[i][j] = "D"
 			}
 
 			if costs[i][j-1]+costInsert < costs[i][j] {
 				costs[i][j] = costs[i][j-1] + costInsert
-				ops[i][j] = 4
+				ops[i][j] = "I"
 			}
 		}
 	}
