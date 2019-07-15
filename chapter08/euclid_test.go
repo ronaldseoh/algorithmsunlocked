b43505c7f1fdc59b52628e8af9f4e1c06f0838ca
Ronald Seoh <ronaldseoh@icloud.com>
2019-07-15 11:10:34 +0900

    chapter08: Euclid: test script

--- chapter08/euclid_test.go
+++ chapter08/euclid_test.go
@@ -10,9 +10,25 @@ func TestEuclid(t *testing.T) {
 	test1 := 40
 	test2 := 64
 
-	g, i, j := Euclid(test1, test2)
+	testExpectedg := 8
+	testExpectedi := -3
+	testExpectedj := 2
 
-	fmt.Println(g)
-	fmt.Println(i)
-	fmt.Println(j)
+	testResultg, testResulti, testResultj := Euclid(test1, test2)
+
+	fmt.Printf("g = %d\n", testResultg)
+	fmt.Printf("i = %d\n", testResulti)
+	fmt.Printf("j = %d\n", testResultj)
+
+	if testResultg != testExpectedg {
+		t.Errorf("Euclid Test - g: Expected %q, Got %q", testExpectedg, testResultg)
+	}
+
+	if testResulti != testExpectedi {
+		t.Errorf("Euclid Test - i: Expected %q, Got %q", testExpectedi, testResulti)
+	}
+
+	if testResultj != testExpectedj {
+		t.Errorf("Euclid test - j: Expected %q, Got %q", testExpectedj, testResultj)
+	}
 }
