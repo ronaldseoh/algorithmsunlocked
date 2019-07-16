218f59b3ad58c22c6aed0319d84f2a6ac3b53157
Ronald Seoh <ronaldseoh@icloud.com>
2019-07-16 23:12:38 +0900

    chapter09: BuildHuffmanTree test

---
+++ chapter09/buildhuffmantree_test.go
@@ -0,0 +1,26 @@
+package chapter09
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/shopspring/decimal"
+)
+
+func TestBuildHuffmanTree(t *testing.T) {
+	testString := "TAATTAGAAATTCTATTATA"
+
+	freq := make(map[string]decimal.Decimal)
+
+	for _, char := range testString {
+		freq[string(char)] = freq[string(char)].Add(
+			decimal.New(1, 0).Div(decimal.New(int64(len(testString)), 0)))
+	}
+
+	testResult := BuildHuffmanTree(testString, freq)
+
+	for i := 0; i < len(testResult.Vertices); i++ {
+		fmt.Println(testResult.Vertices[i].Key.(decimal.Decimal))
+		fmt.Println(testResult.Vertices[i].Value.(string))
+	}
+}
