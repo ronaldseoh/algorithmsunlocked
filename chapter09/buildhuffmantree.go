f201512070721d630285aa5d656ce7ae4f8b0c2c
Ronald Seoh <ronaldseoh@icloud.com>
2019-07-17 11:19:52 +0900

    chapter09: BuildHuffmanTree: comments

--- chapter09/buildhuffmantree.go
+++ chapter09/buildhuffmantree.go
@@ -25,6 +25,8 @@ func BuildHuffmanTree(char string, freq map[string]decimal.Decimal) *chapter06.D
 		Weights:  make(map[*chapter06.Element]map[*chapter06.Element]int),
 	}
 
+	// Each nodes serve as individual trees in the beginning; those nodes are
+	// roots of respective trees.
 	for k, v := range freq {
 		z := &chapter06.Element{
 			Key:   v,
@@ -37,6 +39,9 @@ func BuildHuffmanTree(char string, freq map[string]decimal.Decimal) *chapter06.D
 	}
 
 	for i := 0; i < len(freq)-1; i++ {
+		// Get the two root nodes with the lowest frequencies and combine them.
+		// By the end of this loop, every nodes of the tree will be under
+		// another combined node with frequency == 1.0.
 		x := Q.ExtractMin()
 		y := Q.ExtractMin()
 
@@ -48,6 +53,7 @@ func BuildHuffmanTree(char string, freq map[string]decimal.Decimal) *chapter06.D
 
 		tree.Vertices = append(tree.Vertices, z)
 
+		// Make x and y to be children of the new combined node z.
 		tree.Edges[z] = append(tree.Edges[z], x)
 		tree.Edges[z] = append(tree.Edges[z], y)
 
